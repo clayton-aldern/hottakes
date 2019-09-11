e6d5e697dbf979e17fc3f56c2d39d11772f4517d
clayton-aldern <aldern@caldern.xyz>
2019-09-11 15:21:57 -0700

    add QED, fix regex

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-var words = ["theorem", "conjecture", "postulate", "axiom", "law", "lemma", "corollary", "formula", "proof", "prooves", "QED", "theorems"];
+var words = ["theorem", "conjecture", "postulate", "axiom", "law", "lemma", "corollary", "formula", "proof", "proofs", "prooves", "QED", "Q\\.E\\.D", "theorems"];
 var replacements = ["hot take", "spicy take", "copypasta", "unpopular opinion", "headcanon", "weird flex", "shitpost", "mic drop", "big mood", "nuclear take", "life hack", "galaxy brain", "memegen", "tweetstorm", "sign bunny", "clickbait", "#slatepitch", "snowclone"]
 
 var elements = document.getElementsByTagName('*');
@@ -8,17 +8,18 @@ for (var i = 0; i < elements.length; i++) {
 
     for (w in words){
         var word = words[w];
-        var test = "\\b"+word+"\\b";
+        var test = "\\b"+word+"\\b((?<=D)\\.)?"; // a little funky because of QED v. Q.E.D. v. other cases
         var re = new RegExp(test,"ig");
-        if (word === "proof") {
+        // some special cases
+        if (word === "proof" || word === "proofs") {
             var replacement = "receipts";
         } else if (word === "prooves") {
             var replacement = "has the receipts to show";
-        } else if (word === "QED") {
-            var replacement = "That's the tea.";
+        } else if (word === "QED" || word === "Q\\.E\\.D") {
+            var replacement = "That's the tea";
         } else if (word === "theorems") {
             var replacement = "hot takes";
-        } else {
+        } else { // otherwise, just pick a random replacement
             var replacement = replacements[Math.floor(Math.random()*replacements.length)];
         }
         
@@ -28,7 +29,11 @@ for (var i = 0; i < elements.length; i++) {
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
                 var replacedText = text.replace(re, function(match) {
-                    return matchCase(replacement, match);
+                    if (word === "QED" || word === "Q\\.E\\.D"){
+                        return replacement; // don't match case for the abbreviations
+                    } else {
+                        return matchCase(replacement, match); // otherwise, do
+                    }
                 });
 
                 if (replacedText !== text) {
